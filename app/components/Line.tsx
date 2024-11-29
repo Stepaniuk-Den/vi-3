4b31f00436843db14e2efc11bc1bd4bd82733e9e
StDen <stepaniuk.den@gmail.com>
2024-11-29 11:39:49 +0200

    add icons Language

--- app/components/Line.tsx
+++ app/components/Line.tsx
@@ -1,20 +1,19 @@
 interface ILineProps {
-    className: "marsala-left" | "marsala-center";
-  }
-  
-  const Line: React.FC<ILineProps> = ({ className }) => {
-    return (
-      <span
-        className={`flex w-[260px] h-[2px] my-[20px] bg-customMarsala-accentLight rounded-md ${
-          className === "marsala-left"
-            ? "mr-auto md:ml-auto"
-            : className === "marsala-center"
-            ? "mx-auto"
-            : ""
-        }`}
-      ></span>
-    );
-  };
-  
-  export default Line;
-  
+  className: "marsala-left" | "marsala-center";
+}
+
+const Line: React.FC<ILineProps> = ({ className }) => {
+  return (
+    <span
+      className={`flex w-[100px] h-[4px] my-[20px] bg-customMarsala-accentLight rounded-md ${
+        className === "marsala-left"
+          ? "mr-auto md:ml-auto"
+          : className === "marsala-center"
+          ? "mx-auto"
+          : ""
+      }`}
+    ></span>
+  );
+};
+
+export default Line;
