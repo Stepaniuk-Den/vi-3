892dac0394574e4f72da7db6feaf5ee204eb6b72
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2024-12-16 12:36:34 +0200

    Add DoorsKommerLingPage,xhange PVCSchucoPage

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import clsx from "clsx";
-import { useMessages, useTranslations } from "next-intl";
+import { useLocale, useMessages, useTranslations } from "next-intl";
 import Link from "next/link";
-import { useSelectedLayoutSegment } from "next/navigation";
-import { useRef, useState } from "react";
+import { usePathname} from "next/navigation";
+import { useState } from "react";
 
 interface INavigationItem {
   title: string;
@@ -20,42 +20,37 @@ interface ILinkProps {
 const Navigation = () => {
   const t = useTranslations("Navigation");
   const messages = useMessages();
-  const selectedLayoutSegment = useSelectedLayoutSegment();
+  const pathname = usePathname(); // Отримання поточного шляху
+  const locale = useLocale(); // Отримання поточної мови
   const [hoveredMenu, setHoveredMenu] = useState<string | null>(null);
-  const keys = Object.keys(messages.Navigation);
-  const pathname = selectedLayoutSegment ? `${selectedLayoutSegment}` : "home";
 
-  const otherLinkRef = useRef<HTMLLIElement | null>(null);
+  const keys = Object.keys(messages.Navigation);
+  const selectedLayoutSegment = pathname ? `${pathname.split('/')[1]}` : "home";
 
   const handleMouseEnter = (key: string) => {
     setHoveredMenu(key);
   };
 
-  const handleMouseLeave = (e: React.MouseEvent<HTMLLIElement>) => {
-    if (otherLinkRef.current && e.target !== otherLinkRef.current) {
-      setHoveredMenu(null);
-    }
+  const handleMouseLeave = () => {
+    setHoveredMenu(null);
   };
 
   return (
     <ul className="flex flex-col items-center justify-start h-full gap-x-1 py-3 uppercase lg:flex-row">
       {keys.map((key, index) => {
-        const isActive = pathname === key;
+        const isActive = selectedLayoutSegment === key;
         const item = t.raw(key) as INavigationItem;
         const subKeys = item.links ? Object.values(item.links) : [];
 
         return (
           <li
-            ref={otherLinkRef}
             key={index}
-            className={clsx(
-              "group/item relative flex items-center justify-center min-w-max h-full font-medium  cursor-pointer transition-all"
-            )}
+            className="group/item relative flex items-center justify-center min-w-max h-full font-medium cursor-pointer transition-all"
             onMouseEnter={() => handleMouseEnter(key)}
-            onMouseLeave={(e) => handleMouseLeave(e)}
+            onMouseLeave={handleMouseLeave}
           >
             <Link
-              href={item.href}
+              href={`/${locale}${item.href}`}
               className={clsx(
                 "flex items-center justify-center w-full h-full py-3 px-5 rounded-md border border-transparent transition-all hover:border-white",
                 {
@@ -68,14 +63,14 @@ const Navigation = () => {
             </Link>
             {subKeys.length > 0 && hoveredMenu === key && (
               <div className="absolute top-full pt-3 left-0 flex flex-col w-max items-start">
-                <ul className=" p-2 flex flex-col items-start bg-white rounded-md shadow-md">
+                <ul className="p-2 flex flex-col items-start bg-white rounded-md shadow-md">
                   {subKeys.map((subItem, subIndex) => {
                     return (
                       <li
                         key={subIndex}
                         className="px-4 py-2 rounded-md hover:bg-customMarsala-accent hover:text-white"
                       >
-                        <Link href={`${item.href}/${subItem.slug}`}>
+                        <Link href={`/${locale}${item.href}/${subItem.slug}`}>
                           {subItem.title}
                         </Link>
                       </li>
