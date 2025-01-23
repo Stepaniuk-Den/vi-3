dcf39e9d0df58a5a3b83127a2c1612deba5d5a2e
StDen <stepaniuk.den@gmail.com>
2025-01-23 13:57:18 +0200

    Update page.tsx

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,6 +1,5 @@
 
-import ImageSlider from "@/app/components/ImageSlider";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
 
 type Props = {
@@ -10,14 +9,13 @@ type Props = {
 const ContactsPage: React.FC<Props> = ({ params: { locale } }) => {
   setRequestLocale(locale);
 
-  const t = useTranslations("SlidingDoorsPage");
-  const tMessage = useTranslations("MessageBanner");
-  const tButtons = useTranslations("Buttons");
+  // const t = useTranslations("SlidingDoorsPage");
+  // const tMessage = useTranslations("MessageBanner");
+  // const tButtons = useTranslations("Buttons");
 
   return (
-    <>
-      <ImageSlider />
-    </>
+    <div>
+    </div>
   );
 };
 
