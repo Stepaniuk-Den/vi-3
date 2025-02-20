e97926312cb651f9e0f493dd88d2b547a9b2d1eb
StDen <stepaniuk.den@gmail.com>
2025-02-20 15:31:04 +0200

    change translate

--- app/components/ContactPage/BankCard.tsx
+++ app/components/ContactPage/BankCard.tsx
@@ -6,14 +6,14 @@ interface IProps {
 const BankCard = ({ t }: IProps) => {
   const { title, iban, card } = t
   return (
-    <div className="flex flex-col h-full p-6 gap-4">
+    <div className="flex flex-col h-full p-4 sm:p-6 gap-4">
       <h3 className="titleCl mb-6">{title}</h3>
       <div className="flex">
-        <p className="w-20 sm:w-40">{iban}</p>
-        <p>UA 00 000000 0000000000000000000</p>
+        <p className="w-16 sm:w-1/3 font-roboto">{iban}</p>
+        <p>UA000000000000000000000000000</p>
       </div>
       <div className="flex">
-        <p className="w-20 sm:w-40">{card}</p>
+        <p className="w-16 sm:w-1/3 font-roboto">{card}</p>
         <p>0000 0000 0000 0000</p>
       </div>
     </div>
