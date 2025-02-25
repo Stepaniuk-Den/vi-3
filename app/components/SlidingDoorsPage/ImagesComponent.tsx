65274d49b6d179496c29a30d29105aa527b3c192
StDen <stepaniuk.den@gmail.com>
2025-02-25 19:01:04 +0200

    add dinamic

--- app/components/SlidingDoorsPage/ImagesComponent.tsx
+++ app/components/SlidingDoorsPage/ImagesComponent.tsx
@@ -35,17 +35,6 @@ interface IList {
   | string;
 }
 
-// const getImageDimensionValue = (
-//   dimension: string | { [key: number]: string | undefined } | undefined,
-//   idx: number,
-//   defaultValue: string
-// ): string => {
-//   if (typeof dimension === "object" && dimension !== null) {
-//     return dimension[idx + 1] || defaultValue;
-//   }
-//   return dimension || defaultValue;
-// };
-
 const ImagesComponent: React.FC<IList> = ({
   list,
   width,
